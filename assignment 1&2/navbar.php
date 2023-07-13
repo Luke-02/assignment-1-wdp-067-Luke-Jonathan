73c50ef2fe78bb9163d94e0571d03a6d860757de
Luke-02 <luke.jonathan.chen@gmail.com>
2023-07-13 14:20:30 +0700

    update search bar and css

---
+++ assignment 1&2/navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+          <div class="d-flex justify-content-between">
+            <a class="navbar-brand" href="#">Your Logo</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+            <div class="collapse navbar-collapse" id="navbarContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="main.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="latihan1.html">Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="latihan2.html">Gallery</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="portfolioDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Blog
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="portfolioDropdown">
+                            <a class="dropdown-item" href="FPwBootstrap.html">Portfolio</a>
+                            <a class="dropdown-item" href="index.html">Curriculum Vitae</a>
+                            <a class="dropdown-item" href="#">Project 3</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="latihan5.html">Inventory</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
